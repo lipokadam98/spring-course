5a3c7f5e35106ac6254fc1cc63df836912b0a1f1
lipoka <lipokadam98@gmail.com>
2025-11-27 14:43:45 +0100

    Actuator security demo

---
+++ spring-boot-course-2025/o1-spring-boot-overview/04-actuator-security-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class FunRestController {
+    // expose the "/" endpoint that will return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+    //expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 10k!";
+    }
+
+    //expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
+    }
+}
