05dfed5f07afd4d8d768fbf888f005f2cbe97de4
lipoka <lipokadam98@gmail.com>
2026-01-03 14:57:38 +0100

    Initialize Spring Boot CRUD application with Maven Wrapper, RESTful API for employee management, and MySQL integration.

---
+++ spring-boot-course-2025/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,91 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+
+    private final JsonMapper jsonMapper;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService, JsonMapper jsonMapper) {
+        this.employeeService = employeeService;
+        this.jsonMapper = jsonMapper;
+    }
+
+    // expose "/employees" - return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    // expose "/employees/{employeeId}" - return an employee
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee not found with id: " + employeeId);
+        }
+
+        return employee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // just in case they pass an id in JSON ... set id to 0
+        employee.setId(0);
+        // this is to force a save of a new item... instead of update
+        return employeeService.save(employee);
+    }
+
+    // add mapping for PUT /employees - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    //https://github.com/java-json-tools/json-patch
+
+    // add mapping for PATCH /employees/{employeeId}
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee not found with id: " + employeeId);
+        }
+
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id is not allowed in the request body: " + employeeId);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(employee, patchPayload);
+        return employeeService.save(patchedEmployee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee not found with id: " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Employee deleted successfully with id: " + employeeId;
+    }
+
+}
---
+++ spring-boot-course-2025/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,48 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee theEmployee = null;
+
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }else{
+            //We didn't find the employee
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+        return theEmployee;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
